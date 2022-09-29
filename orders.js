0ddb6c46349174738faf41069cb461b818afc02e
gabrielolisilva <gabrielsilva2799@hotmail.com>
2022-09-29 10:53:52 -0300

    Dashboard project

---
+++ orders.js
@@ -0,0 +1,50 @@
+const Orders = [
+  {
+    productName: "Mini Drone",
+    productNumber: "65588",
+    paymentStatus: "Espera",
+    shipping: "Pendente",
+  },
+  {
+    productName: "Drone KF102",
+    productNumber: "36685",
+    paymentStatus: "Negado",
+    shipping: "Pendente",
+  },
+  {
+    productName: "Drone com câmera",
+    productNumber: "852159",
+    paymentStatus: "Aprovado",
+    shipping: "Entregue",
+  },
+  {
+    productName: "Drone GPS 4k",
+    productNumber: "951357",
+    paymentStatus: "Negado",
+    shipping: "Não Entregue",
+  },
+  {
+    productName: "Drone IF555",
+    productNumber: "654759",
+    paymentStatus: "Aprovado",
+    shipping: "Entregue",
+  },
+  {
+    productName: "Drone UK104",
+    productNumber: "123789",
+    paymentStatus: "Espera",
+    shipping: "Pendente",
+  },
+  {
+    productName: "Drone UF104",
+    productNumber: "684268",
+    paymentStatus: "Aprovado",
+    shipping: "Entregue",
+  },
+  {
+    productName: "Drone GPS 8k",
+    productNumber: "976431",
+    paymentStatus: "Negado",
+    shipping: "Não Entregue",
+  },
+];
